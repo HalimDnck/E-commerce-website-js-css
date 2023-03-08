4e2594336a6007812a5da3f550f01b4650180cea
Halim Dinckalmis <halimdinc0@gmail.com>
2023-03-08 19:27:56 +0300

    version-1

---
+++ home-script.js
@@ -0,0 +1,27 @@
+const deg = 6;
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+})
+
+
+
+
+/*login page*/
+var logpage = document.getElementById('login-page');
+
+window.onclick = function(event) {
+    if (event.target == logpage) {
+        logpage.style.display = "none";
+    }
+}
